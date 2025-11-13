f83cdd71a5b27cf85d15225abd1a57181ca53751
sabsk <sabir.ali.s.k@oracle.com>
2025-11-13 12:19:55 +0530

    initial addition app

---
+++ calculator-app/src/main/java/com/example/Calculator.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
